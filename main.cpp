5487053d8a72324738e6fb7e5669cecd8c00ceb7
james <jameschoong7@gmail.com>
2025-11-29 23:42:55 +0800

    validate user input

--- main.cpp
+++ main.cpp
@@ -4,15 +4,23 @@
 #include "DocumentHandler.h"
 
 using namespace std;
-
+int getValidInput(const string& prompt) {
+    int value;
+    cout << prompt;
+    while (!(cin >> value) || value <= 0) {
+        cout << "Invalid input. Please enter a positive integer: ";
+        cin.clear();
+        cin.ignore(); 
+    }
+    return value;
+}
 int main(){
     srand(static_cast<unsigned>(time(0)));
     int numCertificates, numWords;
 
-    cout << "Enter the number of certificates: ";
-    cin >> numCertificates;
-    cout << "Enter the words per certificate: ";
-    cin >> numWords;
+    numCertificates = getValidInput("Enter the number of certificates: ");
+    numWords = getValidInput("Enter the words per certificate: ");
+    
 
     DocumentHandler<Certificate> handler;
 
